a06b22496bb67bafc6f6a0e7f9cc73f6b27a0c1c
nao20010128nao <nao20010128@gmail.com>
2015-10-06 22:12:25 +0900

    作った

--- src/nao20010128nao/IndicateWin10.php
+++ src/nao20010128nao/IndicateWin10.php
@@ -6,6 +6,8 @@ use pocketmine\plugin\PluginBase;
 use pocketmine\event\Listener;
 use pocketmine\utils\TextFormat;
 
+use pocketmine\event\player\PlayerPreJoinEvent;
+
 class IndicateWin10 extends PluginBase implements Listener
 {
 	public function onEnable(){
@@ -14,5 +16,14 @@ class IndicateWin10 extends PluginBase implements Listener
 		
 	}
 	public function onDisable(){
+	}
+	
+	public function preJoin(PlayerPreJoinEvent $ev){
+		$username=$ev->getPlayer()->getName();
+		$download=file_get_contents("https://account.xbox.com/ja-JP/Profile?GamerTag=".$username);
+		$lower=strtolower($download);
+		if(strpos($lower,$username)){
+			$this->getServer()->broadcastMessage($username." is a Win 10 Ed. player.");
+		}
 	}
 }
